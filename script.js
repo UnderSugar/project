4fed1c651e1a37618bcfaea51eaf020d1ccded11
Andrew <example@gmail.com>
2021-02-14 12:48:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// const result = confirm ( "Are you here?" );
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answers  = [];
+
+// answers[0] = prompt('Как ваше имя?');
+// answers[1] = prompt('Как ваше фамилия?');
+// answers[2] = prompt('Сколько вам лет?');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log('https://someurl.com/${category}/');
+
+// aler
+
+console.log('arr' + "-object");
+console.log(4 + + "5");
+ 
+ 
+let inct = 10, 
+    decr  =10;
+    
